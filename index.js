87fa3168789cc014cedb34288a4ba2089a2c9b85
musicnotes93 <malloryphillipsviolin@gmail.com>
2024-09-03 13:38:55 -0400

    New email server

--- index.js
+++ index.js
@@ -63,7 +63,44 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+const form = document.getElementById('contactForm');
+const result = document.getElementById('result');
 
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "<span>Thank you for your email!<span>";
+            } else {
+                console.log(response);
+                result.innerHTML = "<span>Thank you for your email!<span>";
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 8000);
+        });
+});
 
 
 
