6a4a8990c2b1628310da5ac6a04f2ed7f7743059
musicnotes93 <malloryphillipsviolin@gmail.com>
2024-10-08 15:42:21 -0400

    added react planner app

--- index.js
+++ index.js
@@ -87,36 +87,87 @@ form.addEventListener('submit', function(e) {
 
 
 $( document ).ready(function () {
+
+function updateContent() {
 if($(window).width()<900)
     {
-       $("#website-project").html("<a href='https://www.malloryphillipsviolin.com' target='_blank'><img src='./website.png'</a>");
+       $("#website-project").html("<a href='https://www.malloryphillipsviolin.com' target='_blank'><img src='./website.png'></a>");
        $("#cat-quiz").html("<a href='https://musicnotes93.github.io/CatQuiz/' target='_blank'><img src='./catquiz.png'></a>");
        $("#simon-game").html("<a href='https://musicnotes93.github.io/SimonGameMobileFriendly/' target='_blank'><img src='./simon.png'></a>");
        $("#library").html("<a href='https://musicnotes93.github.io/InteractiveLibrary/' target='_blank'><img src='./library.png'></a>")
-
-    }
-    $(window).resize(function() {
-        if($(window).width()<900) {
-        $("#website-project").html("<a href='https://www.malloryphillipsviolin.com' target='_blank'><img src='./website.png'</a>");
-       $("#cat-quiz").html("<a href='https://musicnotes93.github.io/CatQuiz/' target='_blank'><img src='./catquiz.png'></a>");
-       $("#simon-game").html("<a href='https://musicnotes93.github.io/SimonGameMobileFriendly/' target='_blank'><img src='./simon.png'></a>");
-       $("#library").html("<a href='https://musicnotes93.github.io/InteractiveLibrary/' target='_blank'><img src='./library.png'></a>")
-        }
-
-    })
-})
-
-const vid = document.querySelector('#video');
-let currentTime = 0;
-
-vid.addEventListener('mouseleave', function(e) {
-    if (vid.paused) {
-        currentTime = vid.currentTime;
-        vid.load();
+       $("#planner").html("<a href='https://musicnotes93.github.io/DailyPlanner/' target='_blank'><img src='./reactplanner.png'></a>")
     } else {
-        vid.currentTime = currentTime;
+        $("#website-project").html(`
+                <a href="https://www.malloryphillipsviolin.com" target="_blank">
+                    <video poster="./website.png" id="video" muted loop preload="auto" 
+                        onmouseover="this.play()" onmouseleave="this.pause()">
+                        <source type="video/mp4" src="./phillips.mov">
+                        Your browser does not support the video tag.
+                    </video>
+                </a>
+            `);
+            $("#video").on("mouseleave", function() {
+                this.pause();
+                this.currentTime = 0;
+                this.load(); 
+            });
+        $("#cat-quiz").html('<a href="https://musicnotes93.github.io/CatQuiz/" target="_blank"><video poster="./catquiz.png" id="video2" muted loop preload="auto" onmouseover="this.play()" onmouseleave="this.pause()"><source type="video/mp4" src="./catquiz2.mov"></video></a>');
+        $("#video2").on("mouseleave", function() {
+            this.pause();
+            this.currentTime = 0;
+            this.load(); 
+        });
+        $("#simon-game").html('<a href="https://musicnotes93.github.io/SimonGameMobileFriendly/" target="_blank"><video poster="./simon.png" id="video3" muted loop preload="auto" onmouseover="this.play()" onmouseleave="this.pause()"><source type="video/mp4" src="./simongame2.mov"></video></a>');
+        $("#video3").on("mouseleave", function() {
+            this.pause();
+            this.currentTime = 0;
+            this.load(); 
+        });
+        $("#library").html('<a href="https://musicnotes93.github.io/InteractiveLibrary/" target="_blank"><video poster="./library.png" id="video4" muted loop preload="auto" onmouseover="this.play()" onmouseleave="this.pause()"><source type="video/mp4" src="./libraryvid.mov"></video></a>')
+        $("#video4").on("mouseleave", function() {
+            this.pause();
+            this.currentTime = 0;
+            this.load(); 
+        });
+        $("planner").html('<a href="https://musicnotes93.github.io/DailyPlanner/" target="_blank"><video poster="./reactplanner.png" id="video5" muted loop preload="auto" onmouseover="this.play()" onmouseleave="this.pause()"><source type="video/mp4" src="./reactvid.mov"></video></a>');
+        $("#video5").on("mouseleave", function() {
+            this.pause();
+            this.currentTime = 0;
+            this.load(); 
+        });
     }
+
+}
+updateContent();
+
+$(window).resize(function() {
+    updateContent();
 });
+});
+
+    // $(window).resize(function() {
+    //     if($(window).width()<900) {
+    //     $("#website-project").html("<a href='https://www.malloryphillipsviolin.com' target='_blank'><img src='./website.png'></a>");
+    //    $("#cat-quiz").html("<a href='https://musicnotes93.github.io/CatQuiz/' target='_blank'><img src='./catquiz.png'></a>");
+    //    $("#simon-game").html("<a href='https://musicnotes93.github.io/SimonGameMobileFriendly/' target='_blank'><img src='./simon.png'></a>");
+    //    $("#library").html("<a href='https://musicnotes93.github.io/InteractiveLibrary/' target='_blank'><img src='./library.png'></a>")
+    //    $("#planner").html("<a href='https://musicnotes93.github.io/DailyPlanner/' target='_blank'><img src='./reactplanner.png'></a>")
+    //     } 
+
+    // })
+
+
+// const vid = document.querySelector('#video');
+// let currentTime = 0;
+
+// vid.addEventListener('mouseleave', function(e) {
+//     if (vid.paused) {
+//         currentTime = vid.currentTime;
+//         vid.load();
+//     } else {
+//         vid.currentTime = currentTime;
+//     }
+// });
 
 const vid2 = document.querySelector('#video2');
 
@@ -151,6 +202,16 @@ vid4.addEventListener('mouseleave', function(e) {
         vid4.load();
     } else {
         vid4.currentTime = currentTime;
+    }
+});
+const vid5 = document.querySelector('#video5');
+
+vid5.addEventListener('mouseleave', function(e) {
+    if (vid5.paused) {
+        currentTime = vid.currentTime;
+        vid5.load();
+    } else {
+        vid5.currentTime = currentTime;
     }
 });
 
