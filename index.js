72e16ee32a85d6411bdaa20b12efb44e5163aa08
musicnotes93 <malloryphillipsviolin@gmail.com>
2024-10-23 08:51:06 -0400

    parallax

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 window.addEventListener("DOMContentLoaded", function(){
+    function isInViewport(node) {
+        var rect = node.getBoundingClientRect()
+        return (
+          (rect.height > 0 || rect.width > 0) &&
+          rect.bottom >= 0 &&
+          rect.right >= 0 &&
+          rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+        )
+      }
+
+      $(window).scroll(function() {
+        var scrolled = $(window).scrollTop()
+        $('.parallax-window').each(function(index, element) {
+          var initY = $(this).offset().top
+          var height = $(this).height()
+          var endY  = initY + $(this).height()
+      
+          // Check if the element is in the viewport.
+          var visible = isInViewport(this)
+          if(visible) {
+            var diff = scrolled - initY
+            var ratio = Math.round((diff / height) * 100)
+            $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
+          }
+        })
+      })
+    
+
+ 
+
+
     $("h1").hide().slideDown(1000);
     $("#portfolio-title").hide();
 
@@ -33,11 +65,28 @@ Array.from(listItems).forEach(function(listItem) {
 
 
 
+  $("#scrollUp").hide();
+  const myButton = document.getElementById("scrollUp");
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      myButton.style.display = "block";
+    } else {
+      myButton.style.display = "none";
+    }
+  }
+  
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+  }
 
 
 
-
-
+  
+  
 
 
 document.addEventListener("DOMContentLoaded", () => {
